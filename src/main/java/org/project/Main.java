8f2817b3a7e6a060c05f12d428e7beef502d3eeb
deepanshuoct12 <deepanshu.oct12@gmail.com>
2026-08-19 14:23:30 +0530

    messsage changed

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -4,6 +4,6 @@ package org.project;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("coding task");
     }
 }
